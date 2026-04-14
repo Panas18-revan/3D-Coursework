210cb385e2c4c73c4387a40e71c45c0d85bd326a
Panas18-revan <panaskoirala18@gmail.com>
2026-04-14 14:22:26 +0545

    Added 3rd person,scripts to cars and traffic lights

---
+++ ZebraCrossing/Assets/Scripts/CarController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float speed = 5f;
+    public Transform stopPoint; // where car should stop
+    public TrafficLightController trafficLight;
+
+    private bool shouldStop = false;
+
+    void Update()
+    {
+        CheckTrafficLight();
+
+        if (!shouldStop)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+
+    void CheckTrafficLight()
+    {
+        float distance = Vector3.Distance(transform.position, stopPoint.position);
+
+        if (distance < 5f)
+        {
+            if (trafficLight.currentState == TrafficLightController.LightState.Red ||
+                trafficLight.currentState == TrafficLightController.LightState.Yellow)
+            {
+                shouldStop = true;
+            }
+            else
+            {
+                shouldStop = false;
+            }
+        }
+        else
+        {
+            shouldStop = false;
+        }
+    }
+}
